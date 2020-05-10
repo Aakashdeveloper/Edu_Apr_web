fa05d4e8d9df53a2fd870a946f0b4af42f9e4fe7
aakashDeveloper <ahanda205@gmail.com>
2020-05-10 03:44:36 +0100

    a

--- jquery/weatherapp/weather.js
+++ jquery/weatherapp/weather.js
@@ -1,4 +1,4 @@
-const weatherUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=5&appid=fbf712a5a83d7305c3cda4ca8fe7ef29";
+const weatherUrl = "http://api.openweathermap.org/data/2.5/forecast/daily?q=London&mode=json&units=metric&cnt=5&appid=";
 
 function getWeather(){
     $.ajax({
